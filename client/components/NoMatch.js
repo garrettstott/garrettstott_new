855105beceb9f581919a618cdad083a4010a43c9
garrettstott <garrettstott@gmail.com>
2016-11-07 14:02:51 -0700

    404

--- client/components/NoMatch.js
+++ client/components/NoMatch.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const NoMatch = () => (
-  <div>404</div>
+  <div>
+    <Navbar />
+    <div className='center twelve columns'>
+      <div className='frown mar-top-md'></div>
+      <h1>404</h1>
+      <p className='mar-pad-no'>Oops! It looks like the page you're looking for doesn't exist.</p>
+      <p className='mar-pad-no'>Please use the back button or the navigation to leave this page.</p>
+    </div>
+  </div>
 );
 
 export default NoMatch;
