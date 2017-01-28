0ebe5e4cb2501c4c3c6ab373429ddca964e65833
garrettstott <garrettstott@gmail.com>
2017-01-28 15:29:37 -0700

    home, nav

--- client/components/Home.js
+++ client/components/Home.js
@@ -10,9 +10,8 @@ class Home extends React.Component {
       <div className='home twelve columns'>
         <div className='twelve columns'>
           <div className='garrettstott'></div>
-          <h3 className='home-title'>Garrett Stott</h3>
-          <h5 className="home-subtitle">Full Stack Ruby on Rails Developer</h5>
-          <h5 className="contact-email">garrettstott@gmail.com</h5>
+          <h1 className='home-title'>Garrett Stott</h1>
+          <h4 className="home-subtitle">Software Engineer</h4>
           <div className="contact-icons">
             <a href="https://github.com/garrettstott" target="_blank" title="GitHub">
               <i className="contact-fa fa fa-github"></i>
@@ -20,6 +19,7 @@ class Home extends React.Component {
             <a href="https://www.linkedin.com/in/garrettstott" target="_blank" title="Linkedin">
               <i className="contact-fa fa fa-linkedin-square"></i>
             </a>
+            <h5 className="contact-email">garrettstott@gmail.com</h5>
           </div>
         </div>
       </div>
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -11,6 +11,7 @@ class Navbar extends React.Component {
       <div className='navbar twelve columns clearfix'>
         <Link to='/' title='Home'><div className='navbar-image'></div></Link>
           <ul className='nav-links'>
+            <Link title='Home' to='/'><li>Home</li></Link>
             <Link title='Projects' to='/projects'><li>Projects</li></Link>
             <Link title='About' to='/about'><li>About</li></Link>
             <a href='https://docs.google.com/document/d/1lV6kDY5QZo6vrQsVrEuye0LO0iW8PzRtGSkQJogXJjI/edit?usp=sharing' title='Resume' target='_blank'><li>Resume</li></a>
