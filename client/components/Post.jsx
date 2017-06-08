d3d718af91c08909d0cdc7c9e1d97449f0481f99
garrettstott <garrettstott@gmail.com>
2017-06-08 11:24:45 -0600

    blog titles

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -17,12 +17,12 @@ class Post extends React.Component {
     return (
       <div className='post'>
         <div className='post-info twelve columns'>
-          <div className='post-image'></div>
-          <h5><Link title='Posted by Garrett Stott'className='post-title' to={`/posts/${post.id}`}>{post.title}</Link></h5>
+          <div className='post-image' title='Posted by Garrett Stott'></div>
+          <h5><Link title={post.title} className='post-title' to={`/posts/${post.id}`}>{post.title}</Link></h5>
           <h6 className='post-date'>{date}</h6>
         </div>
         <div dangerouslySetInnerHTML={{__html: body}}></div>
-        <h6 className='center pad-top-lg mar-bot-no'><Link title='Posted by Garrett Stott' className='' to={`/posts/${post.id}`}>Read More</Link></h6>
+        <h6 className='center pad-top-lg mar-bot-no'><Link title='Read More' className='' to={`/posts/${post.id}`}>Read More</Link></h6>
       </div>
     )
   }
